9cd309d11a2d692fd01eea29199ab7ec1038b5ed
Pradnya Katigar <pradnya.katigar007@gmail.com>
2022-05-30 12:22:41 +0530

    Setting env variables and scripts for dev and heroku

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,7 +5,7 @@ const user = require('../models/user')
 const auth = async(req,res,next)=>{
     try {
         const token = req.header('Authorization').replace('Bearer ','')
-        const decoded = jwt.verify(token,'thisismynewcourse')
+        const decoded = jwt.verify(token,process.env.JWT_TOKEN)
         const user = await User.findOne({_id:decoded._id,'tokens.token':token})
         if(!user){
             throw new Error()
